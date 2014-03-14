74a55e6b0495e2dbd7e7d910dc4b29cd1dd92502
Jay Hendren <james.g.hendren@gmail.com>
2014-03-13 22:35:10 -0600

    got server to randomly generate tileracks! :D

--- wordwizard.py
+++ wordwizard.py
@@ -1,22 +1,35 @@
 import webapp2
 import json
+import os
 
+# dev_env = os.environ['SERVER_SOFTWARE'].startswith('Dev')
+
+import scripts
 
 class GetRound(webapp2.RequestHandler):
 
     def get(self):
-        responseobj = {
-            'status': 'fail',
+        # responseobj = {
+        #     'status': 'fail',
+        #     'payload': {
+        #         'tiles': ['a', 'e', 'e', 'l', 'n', 'r', 'r'],
+        #         'words': ['ear', 'earn', 'learn', 'eel', 'earl', 'lee', 'ran', 'reel', 'leer', 'err', 'rare', 'near', 'nearer', 'lane', 'lean', 'leaner', 'relearn']
+        #         }
+        #     }
+
+        rack = scripts.getrack.getrack()
+        responseobj = { 
+            'status': 'ok',
             'payload': {
-                'tiles': ['a', 'e', 'e', 'l', 'n', 'r', 'r'],
-                'words': ['ear', 'earn', 'learn', 'eel', 'earl', 'lee', 'ran', 'reel', 'leer', 'err', 'rare', 'near', 'nearer', 'lane', 'lean', 'leaner', 'relearn']
+                'tiles': rack.tiles,
+                'words': rack.wordlist
                 }
             }
-
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(responseobj))
 
+routes = [
+    ('/api/getRound', GetRound)
+]
 
-application = webapp2.WSGIApplication([
-    ('/api/getRound', GetRound),
-], debug=True)
+application = webapp2.WSGIApplication(routes, debug=True)
