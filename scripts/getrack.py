c491c9e73b6fb46d2b5bfb6c6e6ba55990f70298
Jay Hendren <james.g.hendren@gmail.com>
2014-04-01 11:48:39 -0600

    playing around with json encoding of dictionaries

--- scripts/getrack.py
+++ scripts/getrack.py
@@ -4,6 +4,7 @@
 import random
 import itertools
 import time
+import json
 # import ../models
 
 # globals
@@ -11,66 +12,36 @@ min_tiles = 3
 max_tiles = 8
 min_max_tiles = 7
 
-dictfn    = 'resources/en/dictionaries/sowpods.txt'
-tilesetfn = 'resources/en/tilesets/superscrabble.txt'
+def gettilebag():
+    # get bag of tiles
+    tilesetfn = 'resources/en/tilesets/superscrabble.txt'
+    tilesetf = open(tilesetfn)
+    tilebag = []
+    for line in tilesetf:
+        tile, num = line.strip().split()
+        tile.lower()
+        num = int(num)
+        for i in range(num): tilebag = tilebag + [tile]
+    return tilebag
 
-# open files
-dictf    = open(dictfn)
-tilesetf = open(tilesetfn)
-
-# get dictionary object
-dictobj = {}
-for line in dictf:
-    word = line.strip().lower()
-    letterstr = ''.join(sorted(word))
-    wordlist = dictobj.get(letterstr)
-    if not wordlist:
-        dictobj[letterstr] = []
-        wordlist = dictobj[letterstr]
-    wordlist.append(word)
-
-# get back of tiles
-tilebag = []
-for line in tilesetf:
-    tile, num = line.strip().split()
-    tile.lower()
-    num = int(num)
-    for i in range(num): tilebag = tilebag + [tile]
+def getdictobj():
+    dictfn    = 'resources/en/dictionaries/sowpods.txt'
+    dictf    = open(dictfn)
+    dictobj = {}
+    for line in dictf:
+        word = line.strip().lower()
+        letterstr = ''.join(sorted(word))
+        wordlist = dictobj.get(letterstr)
+        if not wordlist:
+            dictobj[letterstr] = []
+            wordlist = dictobj[letterstr]
+        wordlist.append(word)
+    return dictobj
 
 class Rack():
     def __init__(self, tl=[], wl=[]):
         self.tiles = tl
         self.wordlist = wl
-
-# try generating tile rack on the fly and then finding all words
-def getrack():
-    maxlen = 0
-    rack = []
-    while maxlen < 7:
-        random.shuffle(tilebag)
-        rack = tilebag[0:8]
-        
-        lettersets = {}
-        for i in range(2, 9):
-            for s in itertools.permutations(rack, i):
-                ls = ''.join(s)
-                if ls in lettersets: continue
-                wl = dictobj.get(ls)
-                if wl: lettersets[ls] = wl
-        
-        wordlist = []
-        for ls, wl in lettersets.items():
-            wordlist += wl
-    
-        maxlen = len(max(wordlist, key=lambda x: len(x)))
-        
-    return Rack(rack, wordlist)
-
-def getdict():
-    return dictobj
-
-def gettilebag():
-    return tilebag
 
 class Tilemap():
     def __init__(self):
@@ -105,10 +76,23 @@ class Tilemap():
         if not ntm: return None
         return ntm.get_wordlist(tileset[1:])
 
-tm = Tilemap()
-tm.construct(dictobj)
+    def to_dict(self):
+        result = { "letters": {}, "wordlist": self.wordlist }
+        for l, tm in self.letters.items(): result["letters"][l] = tm.to_dict()
+        return result
 
-def getrack2():
+    def construct_from_dict(self, d):
+        self.wordlist = d["wordlist"]
+        for l, tmd in d["letters"].items():
+            ntm = Tilemap()
+            ntm.construct_from_dict(tmd)
+            self.letters[l] = ntm
+
+    def construct_from_json_file(self, filename):
+        f = open(filename, 'r')
+        self.construct_from_dict(json.load(f))
+
+def getrack2(tilebag, tm):
     st = time.clock()
     maxlen = 0
     rack = []
@@ -125,9 +109,13 @@ def getrack2():
     tt = et - st
     print "total time to make rack:", tt
     print "tileracks tested:", i
-    return Rack(rack, wordlist)
+    r = Rack(rack, wordlist)
+    print r.tiles
+    print r.wordlist
+    return r
     
 if __name__ == '__main__':
+    make_dictobj()
     print "tilemapping created. creating tilerack..."
     for i in range(100):
         rack = getrack2()
